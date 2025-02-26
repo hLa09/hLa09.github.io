8dfe806c836ca1a22368cc9248475bd9a97af5c9
hLa09 <hala09292001@gmail.com>
2025-02-26 00:01:16 -0800

    Update

--- index.js
+++ index.js
@@ -1,24 +1,23 @@
 // This should match the baud rate in your Arduino sketch
 const BAUD_RATE = 9600;
+// Declare global variables
+let port, connectBtn;
 // declare variable to store previous value of x position
 let pX = 0;
 // declare variable to store previous value of y position
 let pY = 0;
-// Declare global variables
-let port, connectBtn;
 
 // This function help setting up the webpage
 function setup() {
   // Run our serial setup function (below)
   setupSerial();
-
   // Create a canvas that is the size of our browser window.
   // windowWidth and windowHeight are p5 variables
   createCanvas(windowWidth, windowHeight);
   // Creating a stroke like density of 4
   strokeWeight(4);
   // Adding stroke line color to distinct from the blackground
-  stroke(rgb(132, 227, 253));
+  stroke('#222222');
   // p5 text settings. BOLD and CENTER are constants provided by p5.
   // See the "Typography" section in the p5 reference: https://p5js.org/reference/
   textFont("system-ui", 50);
@@ -37,19 +36,21 @@ function draw() {
   if (str.length == 0) return;
   // Trim whitespace and split on commas
   let arr = str.trim().split(",");
+  // Make the background white
+  background('rgb(150, 248, 255)');
 
   // Convert each value from analogRead to a number, map it to the joystick range, and round to nearest integer
   // for example of serial passing in: 511, 509, then the value will be convert to the window's range
   const x = round(map(Number(arr[0]), 0, 1023, windowWidth*0.5, windowWidth*0.75));
   const y = round(map(Number(arr[1]), 0, 1023, windowHeight*0.5, windowHeight*0.75));
-  // Make the background black
-  background(0);
   // text white
-  fill(255);
+  fill('rgb(255, 255, 255)');
   // the size of the text display
-  textSize(20);
+  textSize(40);
   // Display text in order of x value, y value on the following coordinates of the window frame
-  text(`${x}, ${y}`, 10, 10, 100, 100);
+  text(`${x}, ${y}`, 750, 55);
+  fill('rgb(61, 36, 0)');
+  circle(x, y, 50);
   // This help creating the line tracker, by loggin the previous position and current position
   console.log(pX);
   // check to see if previous x position is 0 or not
@@ -58,8 +59,7 @@ function draw() {
     pX = x;
     // when prior y position is zero, update the position with the current position
     pY = y;
-  }
-  else{
+  } else{
     // when previous x position is not at 0, create a line that track the range of the position is in
     // previous x to current x, previous y to current y
     line(pX, pY, x, y);
